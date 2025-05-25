79238a895b114da94a3a5652fe3584c132d7e532
Lobotomishka <annrx145@gmail.com>
2025-05-26 00:07:49 +0300

    7 laba is started

---
+++ cblab007/cblab007.cpp
@@ -0,0 +1,22 @@
+﻿using namespace std;
+
+#include <iostream>
+
+struct lapdata			//модель, габаритные размеры (диагональ, толщина), вес, цена
+{
+	string model;
+	struct dimensions
+	{
+		float wig;
+		float dia;
+	};
+	float mass;
+	float prize;
+
+};
+
+int main()
+{
+
+}
+

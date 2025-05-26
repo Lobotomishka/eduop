a9b29b58172a76d2eb1b7e8082fcb1b54cebe67a
Lobotomishka <annrx145@gmail.com>
2025-05-26 11:07:36 +0300

    7 laba in process

--- cblab007/cblab007.cpp
+++ cblab007/cblab007.cpp
@@ -1,22 +1,61 @@
 ﻿using namespace std;
 
+#include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 struct lapdata			//модель, габаритные размеры (диагональ, толщина), вес, цена
 {
 	string model;
 	struct dimensions
 	{
-		float wig;
-		float dia;
+		string wig;
+		string dia;
 	};
-	float mass;
-	float prize;
-
+	string mass;
+	string prize;
+	
 };
+
+void seel(vector <lapdata> laptops)
+{
+	for (int i = 0; i < laptops.size(); i++)
+	{
+		cout << laptops[i].model;
+	}
+}
 
 int main()
 {
+	string line = "Laptopdata.txt";
+	vector <lapdata> laptops;
 
+	ifstream ind(line);
+	if(ind.is_open())
+	{
+		string k;
+		int i = 0;
+		while(ind >> k)
+		{
+			laptops[i].model = k;
+			i++;
+		}
+		ind.close();
+
+		
+	}
+
+
+	//if (ind.is_open())
+	//{
+	//	while (getline(ind, line))
+	//	{
+	//		laptops.push_back(ind);
+	//		cout << line << endl;
+	//	}
+	//}
+	//ind.close();
+	//cout << laptops[0].model << endl;
 }
 
